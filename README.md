7a8393a6059bc8230f6530489c0d9ebf53713728
Dan Cardamore <dan@hld.ca>
2024-03-03 15:25:35 -0500

    basic setup ready for astro

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # blog
-# blog
+
+## Dev Setup Notes
+* Follow instructions from here to setup Nix:
+    https://blog.eigenvalue.net/2024-flake-parts-nodejs-devshell/
--- flake.nix
+++ flake.nix
@@ -19,7 +19,10 @@
   outputs = inputs@{ self, nixpkgs, flake-parts, devshell }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-    imports = [];
+    imports = [
+      ./flake-modules/nodejs-packages.nix
+      ./flake-modules/nodejs-devshell.nix
+    ];
 
     flake = {
       # your existing definitions before using flake-parts...
